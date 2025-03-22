eecdd7fd248663f5a9ec126dac25ed70684d5cce
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-03-23 00:51:13 +0530

    artists

---
+++ constants/playlist_tracks.js
@@ -0,0 +1,155 @@
+export const SONGS = [
+    {
+      rank: '1',
+      track_name: "Ajab Si",
+      artists: [
+        {
+          name: "Vishal-Shekhar",
+          image: "https://i.scdn.co/image/ab6761610000e5eb90b6c3d093f9b02aad628eaf",
+          genres: ["bollywood", "desi", "hindi pop"],
+        },
+        {
+          name: "KK",
+          image: "https://i.scdn.co/image/ab6761610000e5ebb09a31f853166e721d4d46b2",
+          genres: ["bollywood", "hindi pop", "desi"],
+        },
+      ],
+      album_image: "https://i.scdn.co/image/ab67616d0000b273675b3f7dea80153c73581e5e",
+    },
+    {
+      rank: '2',
+      track_name: "Tumse Milke Dil Ka",
+      artists: [
+        {
+          name: "Sonu Nigam",
+          image: "https://i.scdn.co/image/ab6761610000e5ebbc959d7569618ec2af2210f5",
+          genres: [
+            "kannada pop",
+            "bollywood",
+            "sandalwood",
+            "hindi pop",
+            "desi",
+            "marathi pop",
+          ],
+        },
+        {
+          name: "Sabri Brothers",
+          image: "https://i.scdn.co/image/ab6761610000e5eb3aee20d42f0858659437199b",
+          genres: ["qawwali", "sufi", "bollywood", "desi"],
+        },
+      ],
+      album_image: "https://i.scdn.co/image/ab67616d0000b273e7fa423de639247fed12be4a",
+    },
+    {
+      rank: '3',
+      track_name: "Pehli Baar",
+      artists: [
+        {
+          name: "Ajay Gogavale",
+          image: "https://i.scdn.co/image/ab6761610000e5eb27d4de385b256185d1e52279",
+          genres: ["marathi pop", "devotional", "bhajan"],
+        },
+      ],
+      album_image: "https://i.scdn.co/image/ab67616d0000b273757a24699f13a78d2d207aa8",
+    },
+    {
+      rank: '4',
+      track_name: "double take",
+      artists: [
+        {
+          name: "Dhruv",
+          image: "https://i.scdn.co/image/ab6761610000e5eb36b9505153852a85708af75e",
+          genres: [],
+        },
+      ],
+      album_image: "https://i.scdn.co/image/ab67616d0000b273834f16100678d3e800fb5fb9",
+    },
+    {
+      rank: '5',
+      track_name: "Voh Dekhnay Mein",
+      artists: [
+        {
+          name: "Ali Zafar",
+          image: "https://i.scdn.co/image/ab6761610000e5eb4a63849116405206fce30002",
+          genres: ["qawwali", "sufi", "bollywood", "desi pop", "desi", "ghazal"],
+        },
+      ],
+      album_image: "https://i.scdn.co/image/ab67616d0000b273d9168a716dd978d5322f411a",
+    },
+    {
+      rank: '6',
+      track_name: "Kuch To Hua Hai",
+      artists: [
+        {
+          name: "Shankar-Ehsaan-Loy",
+          image: "https://i.scdn.co/image/ab6761610000e5eb3db0499a689aa9b4a47ebba3",
+          genres: ["bollywood", "sufi", "desi", "hindi pop"],
+        },
+        {
+          name: "Alka Yagnik",
+          image: "https://i.scdn.co/image/ab6761610000e5eb935b2dd507212bd72a71d7f9",
+          genres: ["bollywood", "hindi pop", "desi"],
+        },
+        {
+          name: "Shaan",
+          image: "https://i.scdn.co/image/ab6761610000e5eb2573d940f1062a6646891e50",
+          genres: ["bollywood", "hindi pop", "desi", "bangla pop"],
+        },
+      ],
+      album_image: "https://i.scdn.co/image/ab67616d0000b273a2055e0b847ff66fb5206099",
+    },
+    {
+      rank: '7',
+      track_name: "Gulabi",
+      artists: [
+        {
+          name: "Sachin-Jigar",
+          image: "https://i.scdn.co/image/ab6761610000e5ebbd6fbc7d0973cc33940781ad",
+          genres: ["gujarati pop", "bollywood", "hindi pop", "desi", "gujarati garba"],
+        },
+        {
+          name: "Jigar Saraiya",
+          image: "https://i.scdn.co/image/ab6761610000e5eb8980adee05186b3afbbca15a",
+          genres: ["bollywood", "hindi pop", "desi"],
+        },
+        {
+          name: "Priya Saraiya",
+          image: "https://i.scdn.co/image/ab6761610000e5ebefde421035552000de823ad8",
+          genres: ["gujarati pop", "bollywood", "hindi pop", "gujarati garba", "desi"],
+        },
+        {
+          name: "Jaideep Sahni",
+          image: "https://i.scdn.co/image/ab67616d0000b273825ae4fbebb64fd9f4fd7de1",
+          genres: ["bollywood", "desi", "hindi pop", "sufi"],
+        },
+      ],
+      album_image: "https://i.scdn.co/image/ab67616d0000b273b142294d7adcc8e4f599fe87",
+    },
+    {
+      rank: '8',
+      track_name: "Nashe Si Chadh Gayi",
+      artists: [
+        {
+          name: "Vishal-Shekhar",
+          image: "https://i.scdn.co/image/ab6761610000e5eb90b6c3d093f9b02aad628eaf",
+          genres: ["bollywood", "desi", "hindi pop"],
+        },
+        {
+          name: "Arijit Singh",
+          image: "https://i.scdn.co/image/ab6761610000e5eb5ba2d75eb08a2d672f9b69b7",
+          genres: ["hindi pop", "bollywood", "desi", "bangla pop"],
+        },
+        {
+          name: "Caralisa Monteiro",
+          image: "https://i.scdn.co/image/ab6761610000e5ebf43a8d87d4810aa05739b2ef",
+          genres: ["bollywood", "hindi pop", "desi"],
+        },
+        {
+          name: "Jaideep Sahni",
+          image: "https://i.scdn.co/image/ab67616d0000b273825ae4fbebb64fd9f4fd7de1",
+          genres: ["bollywood", "desi", "hindi pop", "sufi"],
+        },
+      ],
+      album_image: "https://i.scdn.co/image/ab67616d0000b273f5dc36d5000145375a41c3b8",
+    },
+  ];  
