f58368163553e38b413dfeba52caf6674b20fa25
Aditya-Chaudhary <adityachaudhary7724@gmail.com>
2025-03-26 17:43:19 +0530

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,17 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-      <Script
-          src="https://cdn.botpress.cloud/webchat/v2.2/inject.js"
-          strategy="beforeInteractive"
-        />
-        <Script
-          src="https://files.bpcontent.cloud/2025/03/20/09/20250320094107-7WLTM8YV.js"
-          strategy="lazyOnload"
-        />
-
-      </head>
+      
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           <AuthProvider>{children}</AuthProvider>
